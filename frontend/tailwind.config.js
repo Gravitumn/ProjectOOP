d70c11da64886241a4c2364cbf03a5c23432b31d
Som3on31 <saharit.f4@gmail.com>
2022-02-27 21:50:22 +0700

    Added a bg for Home.js & config to support it

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'carin': "url('./Pages/Components/images/starto_scr.jpg')",
+        'carin_sm': "url('./Pages/Components/images/starto_scr_sm.jpg')"
+      }
+    },
   },
   plugins: [],
 }
