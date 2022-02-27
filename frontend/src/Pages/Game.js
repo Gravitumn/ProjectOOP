3e2ad960c4e010ff7ecbd12ab6edb1f965dc91c9
Som3on31 <saharit.f4@gmail.com>
2022-02-27 17:17:21 +0700

    Formatted the WIP UI

--- frontend/src/Pages/Game.js
+++ frontend/src/Pages/Game.js
@@ -6,8 +6,31 @@ function Game() {
     let nav = useNavigate()
 
     return (
-        <div>
+        <div >
+            {/* game bar */}
+            <div className="bg-gray-800 flex mx-auto flex-row justify-around text-slate-300">
+                {/* pause */}
+                <div>
+                    pauz
+                </div>
+                {/* virus left */}
+                <div>
+                    10/10
+                </div>
+                <div>
+                    Wave: 3/10
+                </div>
+                <div>
+                    22222
+                </div>
+                <div>
+                    4/5
+                </div>
+            </div>
+
+            
             Game Starto
+
             <button onClick={() => nav('/')}>Back</button>
         </div>
     )
