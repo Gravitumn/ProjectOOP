f50d423e246d11ee01afd57d2371f4661c3d8d65
Som3on31 <saharit.f4@gmail.com>
2022-02-27 17:17:06 +0700

    Formatted code

--- frontend/src/Pages/Four04.js
+++ frontend/src/Pages/Four04.js
@@ -22,11 +22,15 @@ function Four04() {
     return (
         <div className="min-h-screen items-center">
             <div className="flex-col p-6 max-w-sm sm:mx-auto -px-4 bg-white shadow-lg flex items-center content-center space-x-4">
-                <div className="text-3xl font-bold italic underline-offset-2 mb-4">404 not found</div>
+                <div className="text-3xl font-bold italic underline-offset-2 mb-4">404: not found</div>
                 <div>
                     <SadEmoji />
                 </div> {/*images*/}
                 <div>You will be redirected soon.</div>
+                <div classname='bg-gray-400 shadow-lg' onClick={() => {baccToHome()}}>
+                    Or click here to go back
+                </div>
+
                 {/*{home()}*/}    {/*redirect fn here*/}
             </div>
             <div className="text-center">
--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 //icons to be imported
@@ -7,37 +7,70 @@ import { ReactComponent as PlayIdle } from "./Components/images/play_mouzNotOn.s
 
 
 function Home() {
+    //universal warp lol
     let nav = useNavigate()
 
     const [mouzIn, setMouzIn] = useState(false)
 
+    //play button
     const icon = () => {
-        if(mouzIn) return(<Play />)
-        return(<PlayIdle />)
+        if (mouzIn) return (<Play />)
+        return (<PlayIdle />)
+    }
+
+
+    //how to play section - injected in the form of a fn
+    const howToPlay = () => {
+        return (
+            <div className="p-6 mx-4 bg-gray-200">
+                {/* header */}
+                <div className="text-xl">
+                    How to play
+                </div>
+
+                {/* goal */}
+                <div className="mt-6">
+                    Player's goal is to survive a designated number of rounds swarmed by viruses to win.
+                </div>
+
+                {/* shows controls */}
+                <div className="">
+                    Click to deploy, but remember, you have limited funds. Always plan to spend your fund carefully
+
+                    Any antivirus defeated by a virus will turn into one.
+
+                    If you are out of funds and have no antiviruses on the field, it will result in game over.
+
+                    Once a wave is cleared, you gain more funds to deploy more antiviruses.
+                </div>
+            </div>
+        )
     }
 
 
     return (
-        <div>
-            <div class="p-6 max-w-screen mx-4 bg-white shadow-lg flex-col items-center space-x-4">
-                <div>
-                    <div class="text-xl font-medium text-black">Someapp</div>
-                    <p class="text-slate-500">Test</p>
-                </div>
+        <div classname='flex-col justify-center'>
+            <div class="p-6 max-w-screen mx-4 bg-white shadow-lg items-center">
 
-                <div className="flex-row">
-                    <div className="rounded-md" onClick={() => { nav('/game') }} onMouseOver={() => {setMouzIn(true)}} onMouseLeave={() => {setMouzIn(false)}}>
+                <div className="flex flex-row justify-between">
+                    <div className="rounded-md" onClick={() => { nav('/game') }} onMouseOver={() => { setMouzIn(true) }} onMouseLeave={() => { setMouzIn(false) }}>
                         <a href=''>
                             {icon()}
                         </a>
                     </div>
-
-                    <div onClick={() => { nav('*') }}>
-                        404 test
+                    <div className='align-right'>
+                        <div class="text-xl font-medium text-black">Game name here</div>
+                        <p class="text-slate-500">Can you protect your body?</p>
                     </div>
-                </div>
+                    
 
+                </div>
+                {/* 404 test - debug only */}
+                <div onClick={() => { nav('*') }}>
+                    404 test
+                </div>
             </div>
+            {howToPlay()}
             <div classname="text-center">
                 @2022 by abc team
             </div>
@@ -47,22 +80,3 @@ function Home() {
 }
 
 export default Home
-
-{/* <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-                <div class="shrink-0">
-                    <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo"/>
-                </div>
-                <div>
-                    <div class="text-xl font-medium text-black">ChitChat</div>
-                    <p class="text-slate-500">You have a new message!</p>
-                </div>
-            </div> */}
-
-{/* <div className="">
-            
-            <div>
-                <button onClick={() => { nav("/about") }}>About</button>
-                <button onClick={() => { nav("/game") }}>Play</button>
-            </div>
-
-        </div> */}
