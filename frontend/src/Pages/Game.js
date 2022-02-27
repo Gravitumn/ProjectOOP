49095f018363a66d9ee413ad0989bc1d33d52f7c
Som3on31 <saharit.f4@gmail.com>
2022-02-28 00:35:13 +0700

    Added a pause page (incomplete)

--- frontend/src/Pages/Game.js
+++ frontend/src/Pages/Game.js
@@ -1,41 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom'
 import Pauz from './Components/images/pauz.png'
+
+//board to be imported
+import GamePad from "./GamePad";
+
+
 
 
 function Game() {
     let nav = useNavigate()
 
-    return (
-        <div >
-            {/* game bar */}
-            <div className="bg-gray-800 flex mx-auto flex-row justify-around text-slate-300">
-                {/* pause */}
-                <div onClick={() => { }}>
-                    <img src={Pauz} alt='Pause' width='40px' height='40px'/>
+    const [paused, setPaused] = useState(false)
+
+    const page = () => {
+        if (paused) {
+            return (
+                <div className="min-h-screen items-center">
+                    <button className="bg-green-200" onClick={() => {setPaused=(false)}}>
+                        Continue
+                    </button>
+                    <button className="bg-red-200">
+                        Quit
+                    </button>
                 </div>
-                {/* virus left */}
-                <div>
-                    10/10
+            )
+        }
+        return (
+            <div>
+                {/* game bar */}
+                <div className="bg-gray-800 flex mx-auto flex-row justify-around text-slate-300 my-auto items-center">
+                    {/* pause */}
+                    <div onClick={() => { }}>
+                        <button onClick={() => { setPaused(true) }}>
+                            <img src={Pauz} alt='Pause' width='40px' height='40px' />
+                        </button>
+
+                    </div>
+                    {/* virus left */}
+                    <div>
+                        10/10
+                    </div>
+                    {/*wave counter*/}
+                    <div>
+                        Wave: 3/10
+                    </div>
+                    {/*funds*/}
+                    <div>
+                        22222
+                    </div>
+                    {/*lives*/}
+                    <div>
+                        4/5
+                    </div>
                 </div>
-                {/*wave counter*/}
-                <div>
-                    Wave: 3/10
-                </div>
-                {/*funds*/}
-                <div>
-                    22222
-                </div>
-                {/*lives*/}
-                <div>
-                    4/5
-                </div>
+
+
+                <GamePad />
+
+                <button onClick={() => nav('/')}>Back</button>
             </div>
+        )
+    }
 
 
-            Game Starto
-
-            <button onClick={() => nav('/')}>Back</button>
+    return (
+        <div>
+            {page()}
         </div>
     )
 }
---
+++ frontend/src/Pages/GamePad.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function GamePad(){
+    return(
+        <div>
+            Game here
+        </div>
+    )
+}
+
+
+export default GamePad
