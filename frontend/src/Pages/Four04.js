a34dc35d401e5aba673cfd71ed5d94b0cbee117b
Som3on31 <saharit.f4@gmail.com>
2022-02-27 11:08:41 +0700

    Added a time-dalayed redirect fn for Four04

--- frontend/src/Pages/Four04.js
+++ frontend/src/Pages/Four04.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { Route } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
-function Four04(){
-    return(
-        <div>
-            404 not found
+function Four04() {
+    let nav = useNavigate()
+
+    const baccToHome = () => {
+        return (
+            nav('/')
+        )
+    }
+
+    const home = () => {
+        const timer = 5000
+        setTimeout(baccToHome, timer)
+        clearTimeout(timer)
+    }
+
+
+
+    return (
+        <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+            <div className="text-center">404 not found</div>
+            <div>You will be redirected soon.</div>
+            {home()}
         </div>
     )
 }
--- frontend/src/Pages/Game.js
+++ frontend/src/Pages/Game.js
@@ -1,9 +1,9 @@
 import React from "react";
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 
 function Game() {
-    const nav = useNavigate()
+    let nav = useNavigate()
 
     return (
         <div>
